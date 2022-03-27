aa95bed1c89e05bad8cc30706658a2c8f408e498
YAK <yakxuanhong11300@gmail.com>
2022-03-27 15:41:43 +0800

    Week 3 Progress

---
+++ newuser.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.eltes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect( async err => {
+    if (err) {
+        console.log(err.message)
+        return
+    }
+    console.log('Database connected');
+    console.time('Time used'); // start time of operation
+
+     let faker = require("./faker.js");
+     let hash = require("./hash.js");
+     const hashpass = hash.hashpass;
+     
+    client.db('Account').collection('User Details').insertOne({        
+              User: faker.randomName,                           
+              Email: faker.randomEmail,
+              Phone: faker.randomPhoneNumber,
+              Password: hashpass
+            });
+
+    console.log('Operation completed')
+    console.timeEnd('Time used'); // end time of operation
+    
+});

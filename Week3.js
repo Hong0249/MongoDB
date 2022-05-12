58a242da0ecc786fe604d6ddc94f32b3d1444d66
Yak Xuan Hong <77378104+Hong0249@users.noreply.github.com>
2022-05-12 22:25:28 +0800

    Update Week3.js

--- Week3.js
+++ Week3.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.eltes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv-connection-string";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect( async err => {
